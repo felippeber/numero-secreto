98ca52d9e21517754348fb70a6b9ad5fb434563f
felippeber <felippeber@gmail.com>
2023-06-22 17:46:13 -0300

    Atualização do texto

--- app/validacao.js
+++ app/validacao.js
@@ -20,11 +20,11 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         `
     } else if(numero > numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O númeor secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
     } else {
         elementoChute.innerHTML += `
-        <div>O númeor secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
 }
