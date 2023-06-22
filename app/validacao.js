1c94a77fb9accbca35c0c31d0f38734ad01e460b
felippeber <felippeber@gmail.com>
2023-06-22 17:43:30 -0300

    criando arquivo de validação

--- app/validacao.js
+++ app/validacao.js
@@ -1,3 +1,36 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
 
+    if(chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute += `<div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O númeor secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O númeor secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
 }
