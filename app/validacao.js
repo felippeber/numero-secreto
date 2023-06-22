6538dc560a38882e4ae72577d5f79b6d7f7e824a
felippeber <felippeber@gmail.com>
2023-06-22 17:45:26 -0300

    Correção

--- app/validacao.js
+++ app/validacao.js
@@ -1,17 +1,19 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
-    if(chuteForInvalido(numero)) {
+    if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido</div>'
         return
     }
 
-    if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute += `<div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
         return
     }
 
-    if(numero === numeroSecreto) {
+    if (numero === numeroSecreto) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
